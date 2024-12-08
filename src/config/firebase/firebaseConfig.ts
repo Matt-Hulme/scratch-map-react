dac1f1d8553ce892bc25cd760a8d59c0330ffd1f
matt-hulme <mattwhulme@gmail.com>
2024-12-08 15:16:34 -0700

    initial commit

---
+++ src/config/firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCUVu52kQTtFTOLzuZh0a2W_sIdXVze6sc',
+  authDomain: 'scratch-map-7d0bf.firebaseapp.com',
+  projectId: 'scratch-map-7d0bf',
+  storageBucket: 'scratch-map-7d0bf.firebasestorage.app',
+  messagingSenderId: '224895130364',
+  appId: '1:224895130364:web:3209f225470f7986805579',
+  measurementId: 'G-YL6F96MYTV',
+}
