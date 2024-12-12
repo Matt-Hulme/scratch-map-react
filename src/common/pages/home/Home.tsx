3ad7713504715ed7a8d10a41711c1f2e99d2c344
matt-hulme <mattwhulme@gmail.com>
2024-12-11 20:02:01 -0700

    map imported and working!

--- src/common/pages/home/Home.tsx
+++ src/common/pages/home/Home.tsx
@@ -1,9 +1,25 @@
+import { GoogleMap, useLoadScript } from '@react-google-maps/api'
 import { auth } from '../../../config/firebase/firebaseConfig'
 
 export const Home = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
+  })
+
+  if (!isLoaded)
+    return (
+      <div className="bg-[#2e6f40] h-[100vh] flex flex-col items-center justify-center space-y-10 relative">
+        <div>Loading Map...</div>
+      </div>
+    )
+
   return (
     <div className="bg-[#2e6f40] h-[100vh] flex flex-col items-center justify-center space-y-10 relative">
-      <h1 className="text-white font-bold absolute top-10">Scratch-Map</h1>
+      <GoogleMap
+        mapContainerStyle={{ width: '80%', height: '400px' }}
+        center={{ lat: 37.7749, lng: -122.4194 }}
+        zoom={8}
+      />
       <h3 className="text-white">{auth?.currentUser?.displayName}</h3>
     </div>
   )
