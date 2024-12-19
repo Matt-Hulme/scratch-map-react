0e43cc6cf3c3f223d623156a2260818022e89c61
matt-hulme <mattwhulme@gmail.com>
2024-12-18 19:05:42 -0700

    boundaries added, clickable US states

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
-import { Map } from '../../common/Map'
+import { Map } from './Map'
 import { auth } from '../../config/firebase/firebaseConfig'
 
 export const Home = () => {
   return (
     <div className="bg-[#2e6f40] h-[100vh] flex flex-col items-center justify-center space-y-10 relative">
+      <h1 className="text-white text-6xl">Scratch Map</h1>
       <Map />
       <h3 className="text-white">{auth?.currentUser?.displayName}</h3>
     </div>
