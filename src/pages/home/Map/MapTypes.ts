1dc6b6695b62fa4de6b4585d724cf4a197f92673
matt-hulme <mattwhulme@gmail.com>
2025-01-01 22:13:54 -0700

    build fail fix

--- src/pages/home/Map/MapTypes.ts
+++ src/pages/home/Map/MapTypes.ts
@@ -1,6 +1,8 @@
-export interface selectedFeatures {
+interface selectedFeatures {
   ids: string[]
   continents: Set<string>
   countries: Set<string>
   states: Set<string>
 }
+
+export type { selectedFeatures }
